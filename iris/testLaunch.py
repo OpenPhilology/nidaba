df86bb8487d339ee61c6cca3489064c5c3705919
sfrazier-OGL <simon.frazier@informatik.uni-leipzig.de>
2013-08-22 12:35:02 +0200

    Created a module for working with images; added image format validation. Moved the templates folder to /iris.

--- iris/testLaunch.py
+++ iris/testLaunch.py
@@ -4,7 +4,10 @@ from test import imagePreview
 import os
 import logging
 
-
-port = int(os.getenv('PORT', 5000))
 iris = views.startWebViews()
+# print('approot is: ' + 	iris.root_path)
+# iris.root_path = os.path.abspath(os.path.dirname(__file__))
+port = int(os.getenv('PORT', 5000))
+
+
 iris.run(host='0.0.0.0', port=port)
--- iris/web/views.py
+++ iris/web/views.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import imagePreview
+from PIL import Image 
 import os
 import logging
 
@@ -11,40 +12,45 @@ logging.basicConfig(level=logging.DEBUG)
 iris = Flask('iris')
 
 def startWebViews():
-	print('starting webviews')
+	
+	log.debug('starting webviews')
 	return iris
 
 @iris.route('/')
 @iris.route('/index')
 def indexRoute():
     log.debug('routing to the index')
-    return render_template("stubs/index.html")
+    return render_template("index.html")
+
+
+
 
 @iris.route('/batch', methods = ['GET', 'POST'])
 def batchRoute():
-	log.debug('routing to the batch upload portal')
-	log.debug('begin printing headers -----------------------------------')
-	log.debug(request.headers)
-	log.debug('end printing headers -------------------------------------')
-	log.debug(str(len(request.files)) + ' files were upload')
+	# log.debug('routing to the batch upload portal')
+	# log.debug('begin printing headers -----------------------------------')
+	# log.debug(request.headers)
+	# log.debug('end printing headers -------------------------------------')
+	# log.debug(str(len(request.files)) + ' files were upload')
 	file = request.files['photo']
-	imagePreview.previewImage(file)
-	return render_template("stubs/batch.html")
+	imagePreview.imageType(file)
+	return render_template("batch.html")
 
 @iris.route('/batch/<path:specificBatch>', methods = ['GET', 'POST'])
 def specificBatchRoute(specificBatch):	
 	log.debug('routing to specific batch with URN: ' + str('%s' % specificBatch))
-	return render_template("stubs/batch.html", batch = specificBatch)
+	return render_template("batch.html", batch = specificBatch)
+
 
 @iris.route('/collections')
 def collectionRoute():
 	log.debug('routing to collections interface')
-	return render_template("stubs/collections.html")
+	return render_template("collections.html")
 
 @iris.route('/collections/<path:specificCollection>')
 def specificCollectionRoute(specificCollection):
 	log.debug('routing to specific collection with URN: ' + str('%s' + specificCollection))
-	return render_template("stubs/collections.html", collection = specificCollection)
+	return render_template("collections.html", collection = specificCollection)
 
 
 @iris.route('/page')
@@ -54,4 +60,4 @@ def pageRoute():
 @iris.route('/page/<path:pageURN>')
 def specificPageRoute(pageURN):
 	log.debug('routing a specific page of raw OCR output with URN: ' + str('%s' % pageURN))
-	return render_template("stubs/page.html")
+	return render_template("page.html")
