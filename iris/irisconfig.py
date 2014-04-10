5484b656ac811646231c997223ccbec6b44406ad
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-04-10 15:35:38 +0200

    Reformat irisconfig to 80 characters width
    Former-commit-id: 8002a5ef73f9e3042f6e3f2501c3708074140a1c

--- iris/irisconfig.py
+++ iris/irisconfig.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
-#The home directory for Iris to store files created by OCR jobs. For example, tifs, jp2s, meta.xml, and abbyy file downloaded from archive.org are stored here. Each new job is automatically placed in a uniquely named subdir named after its archiveID, creator, and timestamp.
+# The home directory for Iris to store files created by OCR jobs. For example,
+# tifs, jp2s, meta.xml, and abbyy file downloaded from archive.org are stored
+# here. Each new job is automatically placed in a uniquely named subdir named
+# after its archiveID, creator, and timestamp.
 IRIS_HOME = '~/OCR'	
 
-# The prefix of the URL for archive.org downloads, which will have the remainder dynamically appended to it based on a job's archiveID. 
-# For example, if we wish to process archive mechanicaesynta00philgoog, 
-# Iris will generate 'http://www.archive.org/download/mechanicaesynta00philgoog/mechanicaesynta00philgoog_tif.zip' automatically.
+# The prefix of the URL for archive.org downloads, which will have the
+# remainder dynamically appended to it based on a job's archiveID.  For
+# example, if we wish to process archive mechanicaesynta00philgoog, Iris will
+# generate
+# 'http://www.archive.org/download/mechanicaesynta00philgoog/mechanicaesynta00philgoog_tif.zip'
+# automatically.
 ARCHIVE_URL = 'http://www.archive.org/download'
 
 # Storage backend URL. Can be any supported by pyfilesystem, e.g. FTP, RAM
