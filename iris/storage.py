70622dee0f1ffb3e1a42de1ee1c017af9942f5d1
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-05-06 13:39:29 +0200

    New storage module utilizing locking
    Former-commit-id: 8f7d0220b3845b44e2260ce5a4155f9d3ddb0bb6

--- iris/storage.py
+++ iris/storage.py
@@ -1,65 +1,116 @@
 # -*- coding: utf-8 -*-
 # This module contains all file handling/storage management/ID mapping methods
 
-import fs
-from fs import path, errors
-from fs.opener import opener
-from cStringIO import StringIO
+# Not a good way to lock files. Unfortunately flock is an abomination unto the
+# FSM, doesn't work with all NFS implementations and releases all locks on file
+# closure and then reacquires them (non-atomically).
+from lockfile import FileLock
+from os import path
+import irisconfig
+import os
+import fnmatch
 
-# Mounts a/the storage backend from fs_url. Format is described in the
-# configuration file.
-def mount_filestore(fs_url, attempts=10):
-    rfs = None
-    tries = 0
-    while(tries < attempts):
-        try:
-            rfs = opener.opendir(fs_url)
-            if not rfs.getmeta('thread_safe'):
-                pass
-                # FIXME: find out how to to proper celery based logging 
-                # log.warn('Filestore is not thread safe! Make sure to run only'
-                #        'a single celery worker on the queue at all times or'
-                #        'use a thread safe backend.')
-            break
-        except:
-            tries += 1
-    return rfs
 
-# Unmounts a file store
-def umount_filestore(stor):
-    stor.close()
+# Sanitizes a given path with respect to a base path. Returns an absolute path
+# garantueed to be beneath base_path.
+def _sanitize_path(base_path, rel_path):
+    base_path = path.expanduser(base_path)
+    base_path = path.abspath(base_path)
+    rel_path = path.abspath(path.join(base_path, rel_path))
+    if path.commonprefix([path.normpath(rel_path), path.normpath(base_path)]) == base_path:
+        return rel_path
+    else:
+        return ''
 
-# Prepares the filestore to accept data for a job ID. Returns 'None' on
-# failure, job ID on success.
-def prepare_filestore(stor, jobID):
+# Checks if filestore has been prepared for a job.
+def _is_valid_job(jobID):
+    return path.isdir(_sanitize_path(irisconfig.STORAGE_URL, jobID))
+
+def prepare_filestore(jobID):
+    """
+    Prepares the default filestore to accept files for a job. Returns 'None' on
+    failure, job ID on success.
+    """
+    if _is_valid_job(jobID):
+        return None
     try:
-        stor.makedir(jobID)
+        jobPath = _sanitize_path(irisconfig.STORAGE_URL, jobID)
+        os.mkdir(jobPath)
         return jobID
     except Exception as err:
-        # log.debug('Filestore preparation failed for job: ' + jobID)
-        # log.debug(err)
         return None
 
-# Retrieves a list of all files to a job ID
-def list_content(stor, jobID):
-    walk = stor.walkfiles(jobID)
-    return [f[f.find(jobID) + len(jobID) + 1:] for f in walk]
+def list_content(jobID, pattern='*'):
+    """
+    Lists all files to a job ID, optionally applying a glob-like filter.
+    """
+    if not _is_valid_job(jobID):
+        return None
+    flist = []
+    jpath = _sanitize_path(irisconfig.STORAGE_URL, jobID)
+    for root, dirs, files in os.walk(jpath):
+        flist.extend([path.relpath(path.join(root, s), jpath) for s in files])
+    return fnmatch.filter(flist, pattern)
 
-# Returns a dict containing the content of specified doc(s)
-def retrieve_content(stor, jobID, documents=None):
+def retrieve_content(jobID, documents=None):
+    """
+    Retrieves data from a single or a list of documents. Returns binary data,
+    for retrieving unicode text use retrieve_text().
+    """
+    if not _is_valid_job(jobID):
+        return None
     if documents:
         if isinstance(documents, basestring):
             documents = [documents]
-        return {d:stor.getcontents(fs.path.join(jobID, d)) for d in documents}
-    else:
-        return {}
+        fdict = {}
+        dpath = _sanitize_path(irisconfig.STORAGE_URL, jobID)
+        locks = [FileLock(_sanitize_path(dpath, doc)) for doc in documents]
+        # will wait indefinitely until a lock can be acquired.
+        map(lambda x: x.acquire(), locks)
+        for doc in documents:
+            with open(_sanitize_path(dpath, doc), 'rb') as f:
+                fdict[doc] = f.read()
+        map(lambda x: x.release(), locks)
+        return fdict
 
-# Writes a document to a path below a job.
-def write_content(stor, jobID, dest, data):
-    try:
-        stor.setcontents(fs.path.join(jobID, dest), data)
-        return len(data)
-    except:
-        # log.debug('Writing content for job: ' + jobID + ' failed.')
-        # log.debug(err)
+def retrieve_text(jobID, documents=None):
+    """
+    Retrieves UTF-8 encoded text from a single or a list of documents.
+    """
+    res = retrieve_content(jobID, documents)
+    return {t:res[t].decode('utf-8') for t in res}
+
+def write_content(jobID, dest, data):
+    """
+    Writes data to a document at a destination beneath a jobID. Writes bytes,
+    does not accept unicode objects; use write_text() for that.
+    """
+    if not _is_valid_job(jobID):
         return None
+    if not isinstance(data, basestring):
+        return None
+    try:
+        with open(_sanitize_path(irisconfig.STORAGE_URL, path.join(jobID, dest)), 'wb') as f:
+            lock = FileLock(f.name)
+            lock.acquire()
+            f.write(data)
+            lock.release()
+    except:
+        return None
+    return len(data)
+
+def write_text(jobID, dest, text):
+    """
+    Writes text data encoded as UTF-8 to a file beneath a jobID.
+    """
+    return write_content(jobID, dest, text.encode('utf-8'))
+
+## Writes a document to a path below a job.
+#def write_content(stor, jobID, dest, data):
+#    try:
+#        stor.setcontents(fs.path.join(jobID, dest), data)
+#        return len(data)
+#    except:
+#        # log.debug('Writing content for job: ' + jobID + ' failed.')
+#        # log.debug(err)
+#        return None
