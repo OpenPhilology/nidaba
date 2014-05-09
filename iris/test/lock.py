d22bfea6fdf25d1c799074ee898fde5a0e68c516
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-05-09 17:54:09 +0200

    The empty shell that the locking tests are right now
    Former-commit-id: c02d49c2b9b597cc65395e610905172820094fbf

---
+++ iris/test/lock.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+import lock
+import irisconfig
+import uuid
+
+class LockTests(unittest.TestCase):
+    '''
+    Tests the locking implementation.
+    '''
+
+if __name__ == '__main__':
+    unittest.main()
