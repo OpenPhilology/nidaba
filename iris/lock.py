38aa86c2d36dccc9f7aaa945294a62702ba2ca94
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-05-09 17:16:02 +0200

    Add own implementation of symlink based file locking
    Former-commit-id: e6f201e1a8731788e4cdab1c89fff2d9f991d945

---
+++ iris/lock.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# This module contains an NFS-safe locking method that is hopefully
+# interoperable with anything anybody is going to encounter out there.
+
+import os
+import time
+import random
+
+class lock:
+    def __init__(self, locked_file):
+        self._locked_file = locked_file + '.lock'
+        self._lock_file = str(os.uname()) + '.' + str(os.getpid()) + '.lock'
+
+    def acquire(self):
+        while True:
+            try: 
+                os.symlink(self._lock_file, self._locked_file)
+                break
+            except:
+                if os.path.islink(self._locked_file) and \
+                   os.readlink(self._locked_file) == self._lock_file:
+                    break
+            time.sleep(random.random() + 0.001)
+
+    def release(self):
+        if os.path.islink(self._locked_file) and \
+           os.readlink(self._locked_file) == self._lock_file:
+            try:
+                os.remove(self._locked_file) 
+                return True
+            except:
+                pass
+        return False
