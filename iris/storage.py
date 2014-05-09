bcc830f2113a51c53524c1cb55d829fe94ff6b3c
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-05-09 17:26:10 +0200

    Use new file locking thingy
    Former-commit-id: c1567d417c079cfce0245de41edb04622fb2c7e0

--- iris/storage.py
+++ iris/storage.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 # This module contains all file handling/storage management/ID mapping methods
 
-# Not a good way to lock files. Unfortunately flock is an abomination unto the
-# FSM, doesn't work with all NFS implementations and releases all locks on file
-# closure and then reacquires them (non-atomically).
-from lockfile import FileLock
 from os import path
+from lock import lock
 import irisconfig
 import os
 import fnmatch
-
 
 # Sanitizes a given path with respect to a base path. Returns an absolute path
 # garantueed to be beneath base_path.
@@ -23,31 +19,31 @@ def _sanitize_path(base_path, rel_path):
         return ''
 
 # Checks if filestore has been prepared for a job.
-def _is_valid_job(jobID):
-    return path.isdir(_sanitize_path(irisconfig.STORAGE_URL, jobID))
+def is_valid_job(jobID):
+    return path.isdir(_sanitize_path(irisconfig.STORAGE_PATH, jobID))
 
 def prepare_filestore(jobID):
     """
     Prepares the default filestore to accept files for a job. Returns 'None' on
     failure, job ID on success.
     """
-    if _is_valid_job(jobID):
+    if is_valid_job(jobID):
         return None
     try:
-        jobPath = _sanitize_path(irisconfig.STORAGE_URL, jobID)
+        jobPath = _sanitize_path(irisconfig.STORAGE_PATH, jobID)
         os.mkdir(jobPath)
         return jobID
     except Exception as err:
         return None
 
-def list_content(jobID, pattern='*'):
+def list_content(jobID, pattern=u'*'):
     """
     Lists all files to a job ID, optionally applying a glob-like filter.
     """
-    if not _is_valid_job(jobID):
+    if not is_valid_job(jobID):
         return None
     flist = []
-    jpath = _sanitize_path(irisconfig.STORAGE_URL, jobID)
+    jpath = _sanitize_path(irisconfig.STORAGE_PATH, jobID)
     for root, dirs, files in os.walk(jpath):
         flist.extend([path.relpath(path.join(root, s), jpath) for s in files])
     return fnmatch.filter(flist, pattern)
@@ -57,21 +53,21 @@ def retrieve_content(jobID, documents=None):
     Retrieves data from a single or a list of documents. Returns binary data,
     for retrieving unicode text use retrieve_text().
     """
-    if not _is_valid_job(jobID):
+    if not is_valid_job(jobID):
         return None
     if documents:
         if isinstance(documents, basestring):
             documents = [documents]
         fdict = {}
-        dpath = _sanitize_path(irisconfig.STORAGE_URL, jobID)
-        locks = [FileLock(_sanitize_path(dpath, doc)) for doc in documents]
+        dpath = _sanitize_path(irisconfig.STORAGE_PATH, jobID)
+        locks = [lock(_sanitize_path(dpath, doc)) for doc in documents]
         # will wait indefinitely until a lock can be acquired.
         map(lambda x: x.acquire(), locks)
         for doc in documents:
             with open(_sanitize_path(dpath, doc), 'rb') as f:
                 fdict[doc] = f.read()
-        map(lambda x: x.release(), locks)
         return fdict
+        map(lambda x: x.release(), locks)
 
 def retrieve_text(jobID, documents=None):
     """
@@ -85,16 +81,16 @@ def write_content(jobID, dest, data):
     Writes data to a document at a destination beneath a jobID. Writes bytes,
     does not accept unicode objects; use write_text() for that.
     """
-    if not _is_valid_job(jobID):
+    if not is_valid_job(jobID):
         return None
     if not isinstance(data, basestring):
         return None
     try:
-        with open(_sanitize_path(irisconfig.STORAGE_URL, path.join(jobID, dest)), 'wb') as f:
-            lock = FileLock(f.name)
-            lock.acquire()
+        with open(_sanitize_path(irisconfig.STORAGE_PATH, path.join(jobID, dest)), 'wb') as f:
+            l = lock(f.name)
+            l.acquire()
             f.write(data)
-            lock.release()
+            l.release()
     except:
         return None
     return len(data)
