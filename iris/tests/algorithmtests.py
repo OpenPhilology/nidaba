133e9563b64a72774c047b1d460780112a823a78
sfrazier-OGL <simon.frazier@informatik.uni-leipzig.de>
2014-05-12 12:17:24 +0200

    Moved hocr related algorithms (extract_hocr_tokens and extract_bboxes) to the now hocr.py file. Moved tests for said functions to the now hocrtests.py
    Former-commit-id: feaa4b46febd689047eab2997ef62afb32305a8f

--- iris/tests/algorithmtests.py
+++ iris/tests/algorithmtests.py
@@ -868,53 +868,6 @@ class LanguageTests(unittest.TestCase):
         self.assertEqual({greek[0]:len(u'Πλάτων')},
                          algorithms.identify(u'Πλάτων', [greek]))
 
-# ----------------------------------------------------------------------
-# Word Alignment Tests -------------------------------------------------
-# ----------------------------------------------------------------------
-
-class HocrTests(unittest.TestCase):
-    """
-    Tests algorithms dealing with hocr manipulation.
-    """
-
-    def setUp(self):
-        self.temp = tempfile.TemporaryFile()
-
-    def tearDown(self):
-        self.temp.close()
-        
-
-    def test_simple_word_extract(self):
-        self.temp.write("""<html><body><span class='ocrx_word' id='word_10' title="bbox 260 196 305 232">εἰς</span></body><html>""")
-        self.temp.seek(0,0)
-        for t in algorithms.extract_hocr_tokens(self.temp):
-            self.assertEqual('εἰς'.decode('utf-8'), t)
-
-    def test_bbox_extract_simple(self):
-        """"
-        Test hocr bbox extraction in some simple cases.
-        """
-        self.temp.write(u'<root title="bbox 1 2 3 4"></root>')
-        self.temp.seek(0,0)
-        bboxes = algorithms.extract_bboxes(self.temp)
-        self.assertEqual([(1, 2, 3, 4)], bboxes)
-
-    def test_bbox_extract_complex(self):
-        """
-        Test hocr bbox extraction on a larger document which exercises 
-        the bbox identifying regex.
-        """
-        self.temp.write(u"""<root>"""
-                         """<p title="text before bbox 1 2 3 4"></p>"""
-                         """<p title="bbox 5 6 7 8textafter"></p>"""
-                         """<p title="textbeforebbox 9 10 11 12textafter"></p>"""
-                         """</root>""")
-        self.temp.seek(0,0)
-
-        bboxes = algorithms.extract_bboxes(self.temp)
-        self.assertEqual((1, 2, 3, 4), bboxes[0])
-        self.assertEqual((5, 6, 7, 8), bboxes[1])
-        self.assertEqual((9, 10, 11, 12), bboxes[2])
 
 
 if __name__ == '__main__':
