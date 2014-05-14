e7a5d76a0afe849f13a01b682988270193256583
sfrazier-OGL <simon.frazier@informatik.uni-leipzig.de>
2014-05-14 16:07:55 +0200

    Added extract_bboxes_by_class function in hocr, with tests.
    Former-commit-id: fb6d88f541c6b339413c1080cd11e2dc21efbf6b

--- iris/hocr.py
+++ iris/hocr.py
@@ -24,6 +24,8 @@ def extract_hocr_tokens(hocr_file):
     del context
     return words
 
+
+
 def extract_bboxes(hocr_file):
     """
     Extracts a list of bboxes as 4-tuples, in the same order that they
@@ -31,14 +33,33 @@ def extract_bboxes(hocr_file):
     """
     context = etree.parse(hocr_file)
     elements_with_bbox = context.xpath(u'//@title')
+
     pattern = r'.*(bbox{1} [0-9]+ [0-9]+ [0-9]+ [0-9]+)'
     bboxes = []
     for e in elements_with_bbox:
         match = re.match(pattern, str(e))
-        bbox = tuple(map(int, match.groups()[0].decode(u'utf-8')[5:].split(u' ')))
+        bbox = tuple(map(int, match.groups()[0][5:].split(u' ')))
         bboxes.append(bbox)
     return bboxes
 
+def extract_bboxes_by_class(hocrfile, classes):
+    """
+    Extracts bboxes of the specified tags. Returns a dictionary which
+    maps tag names to lists of bboxs. These lists are ordered as the are
+    in the hocr document.
+    """
+    context = etree.parse(hocrfile)
+    results = {hocrclass:[] for hocrclass in classes}
+    pattern = r'.*(bbox{1} [0-9]+ [0-9]+ [0-9]+ [0-9]+)'
+
+    for c in classes:
+        xpath = u"//*[@class='%s' and @title]" % c
+        for element in context.xpath(xpath):
+            match = re.match(pattern, element.attrib[u'title'])
+            bbox = tuple(map(int, match.groups()[0][5:].split(u' ')))
+            results[c].append(bbox)
+
+    return results
 
 def previewbboxs(imgfile, hocrfile, color='blue'):
     """
--- iris/tests/hocrtests.py
+++ iris/tests/hocrtests.py
@@ -48,6 +48,38 @@ class HocrTests(unittest.TestCase):
         self.assertEqual((5, 6, 7, 8), bboxes[1])
         self.assertEqual((9, 10, 11, 12), bboxes[2])
 
+    def test_bbox_extract_by_name(self):
+        """
+        Extract a single class of bboxes.
+        """
+        xml = u"""<root>
+                <p class="c1" title="text before bbox 1 2 3 4"></p>
+                <p class="c1" title="bbox 5 6 7 8textafter"></p>
+                <p class="c1" title="textbeforebbox 9 10 11 12textafter"></p>
+                </root>"""
+        self.temp.write(xml)
+        self.temp.seek(0,0)
+        expected = {u'c1':[(1,2,3,4),(5,6,7,8),(9,10,11,12)]}
+        self.assertEqual(expected, hocr.extract_bboxes_by_class(self.temp, ['c1']))
+
+
+    def test_bbox_extract_by_name_multi(self):
+        """
+        Extract multiple classes of bboxes, while ignoring others.
+        """
+        xml = u"""<root>
+                <p class="c1" title="text before bbox 1 2 3 4"></p>
+                <p class="c2" title="bbox 5 6 7 8textafter"></p>
+                <p class="c3" title="textbeforebbox 9 10 11 12textafter"></p>
+                <p class="whatever" title="13 14 15 16"></p>
+                <p class="more whatever" title="17 18 19 20"></p>
+                </root>"""
+        self.temp.write(xml)
+        self.temp.seek(0,0)
+        expected = {u'c1':[(1,2,3,4)], u'c2':[(5,6,7,8)], u'c3':[(9,10,11,12)]}
+        actual = hocr.extract_bboxes_by_class(self.temp, [u'c1', u'c2', u'c3'])
+        self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
