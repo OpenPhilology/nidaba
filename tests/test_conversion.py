97e1562726563a3a313f0a48ae4dbad16f9fe0cc
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-06-24 13:29:53 +0200

    Fix conversion tests
    Former-commit-id: 7d290fddc0a78fa64c749c0f58e9da29d41c4ebb

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -6,21 +6,24 @@ import difflib
 
 from iris import abbyyToOCR
 
+thisfile = os.path.abspath(os.path.dirname(__file__))
+resources = os.path.abspath(os.path.join(thisfile, u'resources/abbyyConversion'))
+
 #The unit tests for abbyy to hOCR conversion. Each XML to HTML tag transform method is individually tested, then the output of a full conversion is compared against a directory of originals.
 class TestAbbyyToOCRConversion(unittest.TestCase):
     def testConverstion(self):
 
-        abbySample = open('./resources/abbyyConversion/testAbbyy.xml')
+        abbySample = open(os.path.join(resources, u'testAbbyy.xml'))
         tempdir = tempfile.mkdtemp()
-        abbyyToOCR.convert_abbyy_to_ocr(abbySample, tempdir, 'testAbbyy')
+        abbyyToOCR.convert_abbyy_to_ocr(abbySample, tempdir, u'testAbbyy')
 
-        orig_count = len(glob.glob('resources/abbyyConversion/hocr/testHOCR_[0-9][0-9]*.hocr.html')) # And not os.listdir(), which will include crap like .DS_Store.
-        new_count = len(glob.glob(os.path.join(tempdir, 'testAbbyy_[0-9][0-9]*.hocr.html')))
+        orig_count = len(glob.glob(os.path.join(resources, u'hocr/testHOCR_[0-9][0-9]*.hocr.html'))) # And not os.listdir(), which will include crap like .DS_Store.
+        new_count = len(glob.glob(os.path.join(tempdir, u'testAbbyy_[0-9][0-9]*.hocr.html')))
 
         self.assertEqual(orig_count, new_count)
 
         for x in range(0, orig_count):
-            orig_file = 'resources/abbyyConversion/hocr/testHOCR_%i.hocr.html' % x
+            orig_file = os.path.join(resources, u'hocr/testHOCR_%i.hocr.html' % x)
             new_file = os.path.join(tempdir, 'testAbbyy_%i.hocr.html' % x)
             with open(orig_file) as o:
                 with open(new_file) as n:
