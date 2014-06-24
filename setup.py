a2bc69eb56feca9e840fd8d595e8ca1223fbd8e8
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-06-24 11:45:16 +0200

    Use setuptools
    Former-commit-id: db4755b94daf0d006bbc0e51a7fcfcc5578265df

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import glob
+from pip.req import parse_requirements
+from setuptools import setup, find_packages, Extension
+
+# retains all data from the share directory 
+datadir = 'share'
+walker = os.walk(datadir)
+datafiles = [(w[0], [os.path.join(w[0], f) for f in w[2]]) for w in walker]
+
+# All hail the pip-ian way of doing things
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name = "iris",
+    description = "The OCR pipeline to succeed Rigaudon",
+    packages = find_packages(exclude=['tests']),
+    data_files = datafiles,
+    ext_modules = [Extension("deskew", sources=["exts/deskew.c"], libraries=["lept"])],
+    include_package_data=True,
+    test_suite="nose.collector",
+    tests_require="nose",
+    install_requires=reqs,
+    zip_safe = False,
+)
