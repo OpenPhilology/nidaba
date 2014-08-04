dbaf77a777a6f30dc8bbf4a3a568e128a5f47f88
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-08-04 13:39:13 +0200

    test :
    Former-commit-id: 6a98e75cf377156e45f3b057c317f90c6c7c667e

--- iris/tasks.py
+++ iris/tasks.py
@@ -15,6 +15,7 @@ import zipfile
 import requests
 import algorithms
 import leper
+import os
 
 from celery import Celery
 from celery import group
@@ -37,28 +38,34 @@ def ocr_batch(config):
 # on it.
 
 @app.task(name='rgb_to_gray')
-def rgb_to_gray(id=u'', in_files=[], out_dir=u'gray'):
+def rgb_to_gray(id=u'', in_files=[], out_suffix=u'gray'):
     """Converts arbitrary bit depth images in input_dir and writes then into
        the output directory."""
     @app.task(name='ctg_util')
     def ctg_util(input, output):
         return leper.rgb_to_gray(input, output)
-    group([ctg_util.s(storage.get_abs_path(id, input), 
-            storage.get_abs_path(id, out_dir)) for input in in_files])
+    g = group()
+    for input in in_files:
+        (o, e) = os.path.splitext(input)
+        output = o + u'_' + out_suffix + e
+        g |= ctg_util.s(storage.get_abs_path(id, input),
+                storage.get_abs_path(id, output))
+    g.apply_async()
+    
 
 @app.task(name='binarize')
 def binarize(in_files=[], configuration={}):
     """Binarizes a group of input documents and writes them into an output directory. Expects grayscale input"""
 
 @app.task(name='dewarp')
-def dewarp(in_files=[], id=u'', out_dir=u'dewarp'):
+def dewarp(in_files=[], id=u'', out_suffix=u'dewarp'):
     """Removes perspective distortion (as commonly exhibited by overhead scans)
     from images in input_dir and writes them into an output directory.  Expects
     binarized input."""
     pass
 
 @app.task(name='deskew')
-def deskew(in_files=[], id=u'', out_dir=u'deskew'):
+def deskew(in_files=[], id=u'', out_suffix=u'deskew'):
     """Removes skew (rotational distortion) from a set of images in input_dir
     and writes them into an output directory. Expects binarized input."""
     pass
