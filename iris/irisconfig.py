4b73fcfde59ccd8b935dd3e14c759a4af9357de8
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-10-29 14:44:03 +0100

    Fix configuration file for dictionaries
    Former-commit-id: e6ad5343de88574f3d9a981d0e3fc44a14bbffc2

--- iris/irisconfig.py
+++ iris/irisconfig.py
@@ -3,24 +3,13 @@
 # tifs, jp2s, meta.xml, and abbyy file downloaded from archive.org are stored
 # here. Each new job is automatically placed in a uniquely named subdir named
 # after its archiveID, creator, and timestamp.
-IRIS_HOME = '~/OCR'	
+STORAGE_PATH = u'~/OCR'
 
-# The prefix of the URL for archive.org downloads, which will have the
-# remainder dynamically appended to it based on a job's archiveID.  For
-# example, if we wish to process archive mechanicaesynta00philgoog, Iris will
-# generate
-# 'http://www.archive.org/download/mechanicaesynta00philgoog/mechanicaesynta00philgoog_tif.zip'
-# automatically.
-ARCHIVE_URL = 'http://www.archive.org/download'
-
-STORAGE_PATH = '~/OCR'
-
-# Postprocessing configuration
-
-# Spell check configuration
-
-LANG_DICTS = { 'polytonic_greek': 'greek.dic',  'lojban': 'test/lojban.txt', 'german': 'test/german.txt'}
-DICT_PATH = './dictionaries'
+# Spell check configuration. Dictionaries are kept on the common medium (i.e.
+# at STORAGE_PATH/tuple[0]/tuple[1]).
+LANG_DICTS = { u'polytonic_greek': (u'dicts', u'greek.dic'), 
+               u'lojban': (u'dicts', u'test/lojban.txt'), 
+               u'german': (u'dicts', u'test/german.txt')}
 
 # Old tesseract version create hOCR files ending in .html, current ones .hocr
 OLD_TESSERACT = False
