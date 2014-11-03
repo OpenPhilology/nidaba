e391941f6988667f50b3bc03e39a13f3382e2dc1
Benjamin Kiessling <mittagessen@l.unchti.me>
2014-11-03 14:49:39 +0100

    Add download command and remove downloaded files
    Former-commit-id: 711413fa89d0b0e89fcb3b393c0eaeab852e4ab1

--- setup.py
+++ setup.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python
 
 import os
-import glob
+import urllib2
+import urlparse
+import shutil
 from pip.req import parse_requirements
 from setuptools import setup, find_packages, Extension
-
-# retains all data from the share directory 
-datadir = 'share'
-walker = os.walk(datadir)
-datafiles = [(w[0], [os.path.join(w[0], f) for f in w[2]]) for w in walker]
+from distutils.core import setup, Extension, Command
+from distutils.command.install_data import install_data
 
 # All hail the pip-ian way of doing things
 install_reqs = parse_requirements('requirements.txt')
 reqs = [str(ir.req) for ir in install_reqs]
 
+manifest_url = "http://l.unchti.me/iris/MANIFEST"
+download_prefix = "http://l.unchti.me/iris/"
+
+class DownloadCommand(Command):
+    description = "Download misc. data files (dictionaries, sample inputs and models for tests etc.)"
+    user_options = []
+
+    def initialize_options(self): 
+        pass
+
+    def finalize_options(self): 
+        pass
+
+    def run(self):
+        print("Downloading manifest...")
+        manifest = [x.strip() for x in urllib2.urlopen(manifest_url).readlines()]
+        print("Downloading: ")
+        for f in manifest:
+            print('\t* ' + f)
+            try:
+                os.makedirs(os.path.dirname(f))
+            except OSError:
+                pass
+            r = urllib2.urlopen(urlparse.urljoin(download_prefix, f))
+            with open(f, 'wb') as fp:
+                shutil.copyfileobj(r, fp)
+
 setup(
     name = "iris",
     description = "The OCR pipeline to succeed Rigaudon",
     packages = find_packages(exclude=['tests']),
-    data_files = datafiles,
     ext_modules = [Extension("iris.leper", sources=["exts/leper.c"], libraries=["lept"], extra_compile_args=["-std=c99"])],
     include_package_data=True,
     test_suite="nose.collector",
@@ -27,5 +52,8 @@ setup(
     zip_safe = False,
     entry_points = {
         'console_scripts': 'iris = iris.cli:main'
+    },
+    cmdclass = {
+        "download" : DownloadCommand,
     }
 )
