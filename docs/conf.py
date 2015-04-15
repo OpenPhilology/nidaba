fbaf5746a85eafdda0e7c95463481a09066ae721
Benjamin Kiessling <mittagessen@l.unchti.me>
2015-04-15 12:52:05 +0200

    Really add git versioning to sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -3,15 +3,12 @@
 # nidaba documentation build configuration file, created by
 # sphinx-quickstart on Tue Nov  4 18:48:48 2014.
 
-import os
-
 from subprocess import Popen, PIPE
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'celery.contrib.sphinx',
-#    'sphinxcontrib.autoprogram',
 ]
 
 templates_path = ['_templates']
@@ -25,6 +22,7 @@ copyright = u'2014-2015, Open Greek and Latin'
 
 pipe = Popen('git describe --tags --always master', stdout=PIPE, shell=True)
 version = pipe.stdout.read()
+release = version
 
 exclude_patterns = ['_build']
 
@@ -40,5 +38,3 @@ man_pages = [('index', 'nidaba', u'nidaba Documentation', [u'Open Greek and\
 texinfo_documents = [('index', 'nidaba', u'nidaba Documentation', u'Open Greek\
                        and Latin', 'nidaba', 'Expandable and scalable OCR\
                        pipeline', 'Miscellaneous')]
-
-
