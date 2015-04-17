472baa35014224f6618dad5ed28adcbf1c23c062
Benjamin Kiessling <mittagessen@l.unchti.me>
2015-04-17 13:00:12 +0200

    Add plugins_load to configuration file

--- nidaba/plugins/__init__.py
+++ nidaba/plugins/__init__.py
@@ -8,17 +8,17 @@ nidaba.plugins
 from __future__ import absolute_import, print_function
 
 import os
-import importlib
 
-from functools import partial
 from pluginbase import PluginBase
-
 from nidaba.config import nidaba_cfg
 
-# For easier usage calculate the path relative to here.
 
 plugin_base = PluginBase(package='nidaba.plugins')
 plugin_source = plugin_base.make_plugin_source(searchpath=[os.path.dirname(__file__)])
 
 with plugin_source:
-    __import__('nidaba.plugins', globals(), locals(), plugin_source.list_plugins(), -1)
+    if 'plugins_load' in nidaba_cfg:
+        plugin_list = [p['name'] for p in nidaba_cfg['plugins_load']]
+    else:
+        plugin_list = plugin_source.list_plugins()
+    __import__('nidaba.plugins', globals(), locals(), plugin_list, -1)
