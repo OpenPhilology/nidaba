97f83f9c11b3a6256c422c6091af01fdf32600f3
Benjamin Kiessling <mittagessen@l.unchti.me>
2015-05-02 14:25:08 +0200

    New and fancy command line interface

--- nidaba/cli.py
+++ nidaba/cli.py
@@ -10,73 +10,29 @@ from nidaba import Batch, storage
 from nidaba.config import nidaba_cfg
 from nidaba import celery
 from pprint import pprint
+from inspect import getcallargs
 
 import argparse
 import uuid
 import shutil
 import os.path
 import sys
+import click
+import pkg_resources
 
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(pkg_resources.require('nidaba')[0].version)
+    ctx.exit()
+
+@click.group()
+@click.option('--version', is_flag=True, callback=print_version,
+             expose_value=False, is_eager=True)
 def main():
     """
-    Main function called by the hook installed by setuptools.
+    Sends jobs to nidaba and retrieves their status.
     """
-
-    parser = argparse.ArgumentParser(description=u'Sends jobs to nidaba and\
-                                    retrieves their status.',
-                                     epilog=u'This nidaba may or may not have\
-                                    Super Cow Powers.')
-    subparsers = parser.add_subparsers()
-
-    # Command line parameters for querying a job ID
-    statusparser = subparsers.add_parser(
-        'status', help='Displays the status of a job')
-    statusparser.add_argument(
-        'jobid', help='The unique job ID returned by batch.')
-    statusparser.set_defaults(func=status)
-
-    # Command line parameters for configuration file display
-    configparser = subparsers.add_parser(
-        'config', help='Show the current nidaba configuration.')
-    configparser.set_defaults(func=config)
-
-    # Command line parameters for celery workers
-    workerparser = subparsers.add_parser(
-        'worker', help='Run a non-forking celery worker.')
-    workerparser.set_defaults(func=worker)
-
-    # Command line parameters for a new job
-    batchparser = subparsers.add_parser(
-        'batch', help='Put a new batch into the pipeline.')
-    batchparser.add_argument('files', help=u'A list of input files to be converted\
-                              using the pipeline. They will be copied into a\
-                              directory beneath STORAGE_PATH.', nargs='+')
-    batchparser.add_argument('--binarize', help=u'A list of binarization options in\
-                             the format\
-                             algorithm:whsize=10;whsize=20,factor=0.7\
-                             algorithm2:t1,... where algorithm is either otsu\
-                             or sauvola and the parameters are a list of\
-                             particular configuration of the algorithm where\
-                             each configuration is a sequence of algorithmic\
-                             parameters divided by ,.', nargs='+',
-                             default=[u'sauvola:whsize=40'])
-    batchparser.add_argument('--ocr', help=u'A list of OCR engine options in\
-                             the format engine:language1,language2\
-                             engine:model1, model2 where engine is either\
-                             tesseract or ocropus and language* is a tesseract\
-                             language model and model1 is a ocropus model\
-                             defined in the nidaba config.', nargs='+')
-    batchparser.add_argument('--willitblend', help=u'Blends all output files into a\
-                             single hOCR document.', action='store_true',
-                             default=False)
-    batchparser.add_argument('--grayscale', help=u'Input file are already 8bpp\
-                             RGB grayscale.', action='store_true',
-                             default=False)
-
-    batchparser.set_defaults(func=batch)
-
-    args = parser.parse_args()
-    args.func(args)
 
 
 def int_float_or_str(s):
@@ -103,91 +59,139 @@ def int_float_or_str(s):
             except UnicodeDecodeError:
                 return s
 
-
-def batch(args):
+def get_prefix_tasks(prefix=''):
     """
-    Implements the batch subcommand of the nidaba binary.
-
-    Args:
-        args (argparse.Namespace): Parsed input object from argparse
+    Returns a list of all registered tasks (including in plugins) that start
+    with a certain prefix, i.e. are 
     """
+    t = celery.app.tasks
+    return [k.split('.')[-1] for k in t.iterkeys() if k.startswith('nidaba.' +
+            prefix)]
 
-    id = unicode(uuid.uuid4())
+def validate_definition(ctx, param, value):
+    """
+    Validates all task definitions of a group and returns them as a list.
+    """
+    definitions = []
+    for alg in value:
+        (task, _, params) = alg.partition(u':')
+        task = param.human_readable_name + '.' + task
+        if 'nidaba.' + task not in celery.app.tasks:
+            raise click.BadParameter('Unknown task ' + task)
+        configurations = []
+        for conf in params.split(u';'):
+            args = []
+            kwargs = {}
+            for arg in conf.split(u','):
+                # treat as kwarg
+                if '=' in arg:
+                    k, v = arg.split('=')
+                    kwargs[k] = int_float_or_str(v)
+                # treat as arg as long as no kwargs were defined
+                elif not kwargs and len(arg):
+                    args.append(int_float_or_str(arg))
+                elif not len(arg):
+                    continue
+                else:
+                    raise click.BadParameter('Positional argument after '
+                                             'keyword argument')
+            try:
+                fun = celery.app.tasks['nidaba.' + task]
+                # fill in common kwargs (doc/method)
+                args.insert(0, '')
+                args.insert(0, '')
+                kwargs = getcallargs(fun.run, *args, **kwargs)
+                kwargs.pop('doc')
+                kwargs.pop('method')
+            except TypeError as e:
+                raise click.BadParameter(e.message)
+            configurations.append(kwargs)
+        definitions.append([task, configurations])
+    return definitions
+
+@main.command()
+@click.argument('files', type=click.Path(exists=True), nargs=-1, required=True)
+@click.option('--binarize', '-b', multiple=True, callback=validate_definition)
+@click.option('--ocr', '-o', multiple=True, callback=validate_definition, 
+              help='A list of OCR engine options in '
+              'the format engine:language1,language2 engine:model1, model2 '
+              'where engine is either tesseract or ocropus and language* is a '
+              'tesseract language model and model1 is a ocropus model defined '
+              'in the nidaba config.')
+@click.option('--willitblend/--noblend', 'blend',  default=False, help='Blend '
+              'all output files into a single hOCR document.')
+@click.option('--grayscale/--color', default=False, help='Skip grayscale '
+              'conversion using the ITU-R 601-2 luma transform if the input '
+              'documents are already in grayscale.')
+@click.option('--erate', type=click.Path(exists=True), help='Calculate the'
+              'error rate using a ground truth.')
+@click.option('--jobid', default=uuid.uuid4(), type=str, help='Force a job '
+              'identifier. This may or may not be an UUID but it has to be an '
+              'unused identifer.')
+def batch(files, binarize, ocr, blend, grayscale, erate, jobid):
+    """
+    Add a new job to the pipeline.
+    """
+    id = unicode(jobid)
     batch = Batch(id)
-    print('Preparing filestore....', end=''),
+    click.echo(u'Preparing filestore\t\t[', nl=False),
     if storage.prepare_filestore(id) is None:
-        print('failed.')
+        click.secho(u'\u2717', fg='red', nl=False)
+        click.echo(']')
         exit()
-    for doc in args.files:
+    for doc in files:
         shutil.copy2(doc, storage.get_abs_path(id, os.path.basename(doc)))
         batch.add_document((id, os.path.basename(doc)))
-    print('done.')
-    print('Building batch...', end='')
-
+    click.secho(u'\u2713', fg='green', nl=False)
+    click.echo(']')
+    click.echo(u'Building batch\t\t\t[', nl=False)
     batch.add_step()
-    if not args.grayscale:
+    if not grayscale:
         batch.add_tick()
         batch.add_task('img.rgb_to_gray')
-    if args.binarize:
+    if binarize:
         batch.add_tick()
-        for bin in args.binarize:
-            (alg, _, params) = bin.partition(u':')
-            for c in params.split(u';'):
-                kwargs = dict(kwarg.split('=') for kwarg in c.split(",") if
-                              len(kwarg.split('=')) == 2)
-                kwargs = {key: int_float_or_str(val)
-                          for key, val in kwargs.items()}
-                batch.add_task('binarize.' + alg, **kwargs)
-    if args.ocr:
+        for alg in binarize:
+            for kwargs in alg[1]:
+                batch.add_task(alg[0], **kwargs)
+    if ocr:
         batch.add_tick()
-        for ocr in args.ocr:
-            (engine, _, params) = ocr.partition(u':')
-            if engine == u'tesseract':
-                batch.add_task('ocr.tesseract', languages=params.split(u','))
-            elif engine == u'ocropus':
-                for model in params.split(u','):
-                    if model not in nidaba_cfg['ocropus_models']:
-                        print('WARNING: ocropus model ' +
-                              model.encode('utf-8') + ' not known.')
-                    else:
-                        batch.add_task('ocr.ocropus', model=model)
-            else:
-                print('WARNING: OCR engine ' + engine.encode('utf-8') + ' not\
-                      known.')
-    if args.willitblend:
+        for alg in ocr:
+            for kwargs in alg[1]:
+                batch.add_task(alg[0], **kwargs)
+    if blend:
         batch.add_step()
         batch.add_tick()
         batch.add_task('util.blend_hocr')
     batch.run()
-    print('done.')
-    print(id)
+    click.secho(u'\u2713', fg='green', nl=False)
+    click.echo(']')
+    click.echo(id)
 
 
-def worker(args):
+@main.command()
+def worker():
     """
-    Implements the worker subcommand.
+    Starts a celery worker.
     """
-    sys.argv.pop(-1)
-    celery.app.worker_main()
+    celery.app.worker_main(argv=sys.argv[:1])
 
 
-def config(args):
+@main.command()
+def config():
     """
-    Implements the config display subcommand.
+    Displays the current configuration.
     """
-
     pprint(nidaba_cfg)
 
 
-def status(args):
+@main.command()
+@click.argument('job_id', nargs=1, type=str)
+def status(job_id):
     """
-    Implements the status subcommand.
-
-    Args:
-        args (argparse.Namespace): Parsed input object from argparse
+    Diplays the status and results of jobs.
     """
-
-    batch = Batch(args.jobid)
+    batch = Batch(job_id)
     state = batch.get_state()
     print(state)
     if state == 'SUCCESS':
