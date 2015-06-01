0b3f1a97bf57c2482a98259ecafe5eb59e70e83c
Benjamin Kiessling <mittagessen@l.unchti.me>
2015-06-01 14:55:20 +0200

    Add task for spell checking

--- nidaba/tasks/postprocessing.py
+++ nidaba/tasks/postprocessing.py
@@ -9,10 +9,49 @@ Various postprocessing tasks that operate upon recognized texts.
 
 from __future__ import absolute_import, unicode_literals
 
-from nidaba.tasks.helper import NidabaTask
-from nidaba.celery import app
 from nidaba import storage
 from nidaba import merge_hocr
+from nidaba import lex
+from nidaba.tasks.helper import NidabaTask
+from nidaba.celery import app
+from nidaba.config import nidaba_cfg
+
+
+@app.task(base=NidabaTask, name=u'nidaba.postprocessing.spell_check')
+def spell_check(doc, method=u'spell_check', language=u'',
+                filter_punctuation=False, no_ocrx_words=u'auto'):
+    """
+    Adds spelling suggestions to an hOCR document. 
+
+    Alternative spellings for each hocr ``ocrx_word`` span are created using
+    the INS-DEL syntax defined for alternative readings in the hOCR
+    specification. Correct words, i.e. words appearing verbatim in the
+    dictionary, are left untouched; words not appearing in the dictionary and
+    without suggestions will still be encoded as a 
+
+    Args:
+        doc (unicode, unicode): The input document tuple.
+        method (unicode): The suffix string appended to the output file.
+        language (unicode): Identifier defined in the nidaba configuration as a
+                            valid dictionary.
+        filter_punctuation (bool): Switch to filter punctuation inside
+                                   ``ocrx_words``
+        no_ocrx_words (unicode): Fallback switch to extract words from on hOCR
+                                 document without ``ocrx_word`` elements. May
+                                 be set to ``auto``, ``true``, or ``false``.
+
+    Returns:
+        (unicode, unicode): Storage tuple of the output document
+    """
+    input_path = storage.get_abs_path(*doc)
+    output_path = storage.insert_suffix(input_path, method, language,
+                                        filter_punctuation, no_ocrx_words)
+    dictionary = nidaba_cfg['lang_dicts'][language][0]
+    del_dictionary = nidaba_cfg['lang_dicts'][language][1]
+    ret = lex.hocr_spellcheck(input_path, dictionary, del_dictionary,
+                              filter_punctuation, no_ocrx_words)
+    storage.write_text(*get_storage_path(output_path), text=ret)
+    return get_storage_path(output_path)
 
 
 @app.task(base=NidabaTask, name=u'nidaba.postprocessing.blend_hocr')
