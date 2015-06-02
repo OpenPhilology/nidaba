2df70f591248d05de8a2e9e6eb101cf221362138
Benjamin Kiessling <mittagessen@l.unchti.me>
2015-06-02 12:51:51 +0200

    Extras to setup.cfg

--- setup.py
+++ setup.py
@@ -5,8 +5,5 @@ from setuptools import setup
 setup(
     setup_requires=['pbr', 'testrepository', 'nose>=1.0', 'mock>=1.0'],
     pbr=True,
-    extras_require = {
-        'kraken': ['kraken>=0.3.1']
-    },
 )
 
