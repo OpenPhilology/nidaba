16418ec37dbe1c3f5d27ac9f2eb8f1b1f7b35e41
Benjamin Kiessling <mittagessen@l.unchti.me>
2016-11-15 21:40:51 +0100

    pybossa plugin

--- nidaba/plugins/pybossa.py
+++ nidaba/plugins/pybossa.py
@@ -2,63 +2,44 @@
 """
 nidaba.plugins.pybossa
 ~~~~~~~~~~~~~~~~~~~~~~
-
-Adds an pybossa interface.
-
-Run:
-
-    $ pip install pybossa-client
-
-and set server, apikey, and project in the configuration file.
 """
 
 from __future__ import unicode_literals, print_function, absolute_import
 
-import os
-import shutil
-import regex
-import requests
-
-from nidaba import api
 from nidaba import storage
-from flask_restful import url_for
+from nidaba.celery import app
+from nidaba.nidabaexceptions import NidabaPluginException
+from nidaba.tasks.helper import NidabaTask
+
+from celery.utils.log import get_task_logger
 
 logger = get_task_logger(__name__)
 
 
 def setup(*args, **kwargs):
-    global pbclient
-    import pbclient
-    pbclient.set('endpoint', kwargs.get(u'server'))
-    pbclient.set('api_key', kwargs.get(u'apikey'))
-    global pybossa_project
-    pybossa_project = pbclient.find_project(short_name=kwargs.get(u'project'))[0]
-    global pybossa_app
-    # XXX: request independent URL generation requires an explicit server name.
-    # Ideally we'd get it from somewhere else.
-    global pybossa_app
-    pybossa_app = api.create_app()
-    pybossa_app.config['SERVER_NAME'] = kwargs.get(u'nidaba_server')
+    try:
+        global pbclient
+        import pbclient
+        pbclient.set('endpoint', kwargs.get('server'))
+        pbclient.set('api_key', kwargs.get('api_key'))
+        global project
+        project = pbclient.find_project(short_name=kwargs.get('project'))[0]
+    except Exception as e:
+        raise NidabaPluginException(e.message)
 
 
-@app.task(base=NidabaTask, name=u'nidaba.archive.pybossa')
-def archive_pybossa(doc, method=u'pybossa'):
+@app.task(base=NidabaTask, name=u'nidaba.output.pybossa')
+def output_pybossa(doc, method=u'output_pybossa'):
     """
-    "Archives" the output by creating tasks on a remote pybossa instance.
+    Adds recognition result to a pybossa service for postcorrection.
 
     Args:
         doc (unicode, unicode): The input document tuple
-        method (unicode): The suffix string append to all output files
+        method (unicode): The suffix string appended to all output files.
 
     Returns:
-        The input storage tuple
+        The input storage tuple.
     """
-
-    input_path = storage.get_abs_path(*doc)
-    with pybossa_app.app_context():
-        logger.debug('Creating task {} {} on pybossa'.format(*doc))
-        pbclient.create_task(pybossa_project, {
-                             'batch_id': doc[0],
-                             'xml': url_for('api.page', batch=doc[0], file=doc[1])
-                            })
+    logger.debug('Creating pybossa task {} {}'.format(*doc))
+    pbclient.create_task(project, {'batch_id': doc[0], 'xml': storage.get_url(*doc)})
     return doc
