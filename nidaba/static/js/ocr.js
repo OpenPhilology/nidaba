f6b8cc8fd4ee169b1d30556e028ede2ea3759c39
Benjamin Kiessling <mittagessen@l.unchti.me>
2016-11-16 20:23:04 +0100

    Add png conversion task to web frontend

--- nidaba/static/js/ocr.js
+++ nidaba/static/js/ocr.js
@@ -367,6 +367,7 @@ Iris.Views.PreProcess = Backbone.View.extend({
 		$('#submit-batch').on('click', function(e) {
 			console.log('submitting batch');
 			if(Iris.batch.tasks.length == 0) {
+				Iris.batch.add_task('img', 'any_to_png', {});
 				Iris.batch.add_task('binarize', 'nlbin', {threshold: 0.5, 
 									  zoom: 0.5, 
 									  escale: 1.0, 
